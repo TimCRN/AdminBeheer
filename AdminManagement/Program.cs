1f3a38da9e97556b34df14e3979732c2405badf7
Tim <tim.crounse@gmail.com>
2021-05-17 13:36:17 +0200

    Docker Compose port fix

--- AdminManagement/Program.cs
+++ AdminManagement/Program.cs
@@ -17,23 +17,11 @@ namespace AdminManagement
         {
             var host = CreateHostBuilder(args).Build();
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<UserContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
+            CreateDbIfNotExists(host);
 
             host.Run();
         }
+
         private static void CreateDbIfNotExists(IHost host)
         {
             using (var scope = host.Services.CreateScope())
