91291f650e2befc4536762acda094b54510e7257
Tim <tim.crounse@gmail.com>
2021-05-19 23:12:22 +0200

    Initial setup for ViewModels

---
+++ AdminManagement/ViewModels/UserCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AdminManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminManagement.ViewModels
+{
+    public class UserCreateViewModel
+    {
+        public int RoleId { get; set; }
+        public Role[] AvailableRoles { get; set; }
+
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
